7bce05564ccb7374843705cd5dfd74920d711eba
estebanba <esteban.ba@gmail.com>
2022-08-10 10:29:50 +0200

    add netlify file with working redirect

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
             <Route path="/work" element={<WorkPage />} />
             <Route path="/hireme" element={<HireMePage />} />
             <Route path="/contact" element={<ContactSection />} />
+            {/* <Route path="*" element={<HomePage />} /> */}
           </Routes>
         </Layout>
       </WorkContextProvider>
